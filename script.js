dfb328cd2b9079e6f2eddf7aa049c788be9d8e21
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-06-13 14:09:35 +0530

    swtiching themes on toggle swtich

---
+++ script.js
@@ -0,0 +1,18 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+function swtichTheme(event) {
+  console.log(event.target.checked);
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+}
+
+toggleSwitch.addEventListener('change', swtichTheme);
