6e9fd7d4abeda0f9dc0777b038219488324f4820
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-06-13 15:00:19 +0530

    local storage to user data

--- script.js
+++ script.js
@@ -31,15 +31,27 @@ function lightMode() {
   imageMode('light');
 }
 
+// Switch themes dynamically
 function swtichTheme(event) {
-  console.log(event.target.checked);
   if (event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
     darkMode();
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
     lightMode();
   }
 }
 
 toggleSwitch.addEventListener('change', swtichTheme);
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
